1efedd74f2b2cbf118782745806dc0b035fd637f
VictorDQuintero <131626735+VictorDQuintero@users.noreply.github.com>
2024-10-26 23:27:41 -0600

    Add pseudo code

---
+++ math_class.rb
@@ -0,0 +1,19 @@
+# Math class should have a random question definition, which prints the question. Gets user input and evaluates its truth value.
+
+class Math 
+
+  def random_question
+  # generates question
+  # saves answer to the question
+  # puts it out there
+  end
+
+  def evaluates 
+    # chomps answer
+    # evaluates user answer against saved answer
+    # saves evaluation result
+  end
+
+  # method or def for sending the evaluation to game object
+
+end
